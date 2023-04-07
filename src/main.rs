aab055a5f2051f22a4101d8de739657dbe7d2b7f
agrofrank <franco.mangone@agrotoken.io>
2023-04-07 17:46:56 -0300

    Starts playing around with Board struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::collections::HashMap;
+
+struct Board {
+    pieces: HashMap<String, String>
+}
+
+impl Board {
+    // Creates a new board struct
+    fn new() -> Board {
+        Board {
+            pieces: HashMap::new()
+        }
+    }
+
+    // Adds a piece to an existing board
+    // TODO: Consider using some sort of encoding for the position
+    fn add_piece(&mut self, position: String, piece: String) {
+        // TODO: Check if piece is already present!
+        self.pieces.insert(position, piece);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut board = Board::new();
+    println!("A board was created! It still has no pieces inside.");
+
+    board.add_piece(String::from("1-1"), String::from("PAWN"));
+    println!("A pawn was added!");
+
+    println!("We should now see that in the board instance: {:?}", board.pieces);
 }
