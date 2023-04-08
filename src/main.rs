7a733689b0fba3e78c2c9cf0a50eb10592c00c62
agrofrank <franco.mangone@agrotoken.io>
2023-04-08 14:43:24 -0300

    Adds a check for out of bounds, and changes debug implementation

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,19 @@
 use std::collections::HashMap;
+use std::fmt;
+
+#[derive(Clone, Copy, PartialEq, Eq, Hash)]
+struct Position(u8, u8); // Positions are zero-indexed
+struct Dimensions(u8, u8);
+
+impl fmt::Debug for Position {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "({}, {})", self.0, self.1)
+    }
+}
 
 struct Board {
-    pieces: HashMap<String, String>
+    pieces: HashMap<Position, String>,
+    dimensions: Dimensions,
 }
 
 enum BoardError {
@@ -10,38 +22,51 @@ enum BoardError {
 }
 
 impl Board {
+    //
     // Creates a new board struct
-    fn new() -> Board {
+    fn new(rows: u8, cols: u8) -> Board {
         Board {
-            pieces: HashMap::new()
+            pieces: HashMap::new(),
+            dimensions: Dimensions(rows, cols),
         }
     }
 
+    //
     // Adds a piece to an existing board
     // TODO: Consider using some sort of encoding for the position
-    fn add_piece(&mut self, position: &str, piece: &str) -> Result<(), BoardError> {        
+    fn add_piece(&mut self, position: &Position, piece: &str) -> Result<(), BoardError> {        
         // Existing cannot place a piece in place of another (revisit this).
         if self.pieces.contains_key(position) {
             return Err(BoardError::TileNotEmpty);
         }
 
-        // TODO: Out of bounds!
+        // Check if position is out of bounds
+        if self.dimensions.0 <= position.0 || self.dimensions.1 <= position.1 {
+            return Err(BoardError::OutOfBounds);
+        }
 
-        self.pieces.insert(String::from(position), String::from(piece));
+        self.pieces.insert(*position, String::from(piece));
         Ok(())
     }
 }
 
 fn main() {
-    let mut board = Board::new();
+    let mut board = Board::new(8, 8);
     println!("A board was created! It still has no pieces inside.");
 
-    board.add_piece("2-1", "PAWN").ok();
-    board.add_piece("1-1", "PAWN").ok();
+    let pos1 = Position(1,1);
+    let pos2 = Position(2,1);
+    let pos3 = Position(8,0);
+
+    board.add_piece(&pos1, "PAWN").ok();
+    board.add_piece(&pos2, "PAWN").ok();
     println!("A couple pawns were added! (2-1, 1-1)");
     
     println!("If we want to add a piece in an existing place, that should return an error and not alter state.");
-    board.add_piece("1-1", "PAWN").ok();
+    board.add_piece(&pos2, "PAWN").ok();
+
+    println!("Also, adding a piece out of bounds doesn't work.");
+    board.add_piece(&pos3, "PAWN").ok();
 
     println!("We should now see only two pawns in the board instance: {:?}", board.pieces);
 }
